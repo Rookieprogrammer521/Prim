4e6bad26039b3baacf3f4a2b02e437859fe9cc5e
deus <not.entari@gmail.com>
2014-05-18 18:46:41 +0300

    Created dummy functions (empty)

--- Prim.py
+++ Prim.py
@@ -1,1 +1,26 @@
-print ("labas")
+def weight():
+    
+    pass
+
+
+def adjacent(A):
+
+    pass
+
+
+def extract_min(Q):
+
+    pass
+
+
+def decrease_key(Q):
+
+    pass
+
+
+def prim(V):
+
+    pass
+
+
+print ("galutinis rezultatas")
