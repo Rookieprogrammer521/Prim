e3192ec9fecfd15fbcb04b4b4a2b9d1a9e9db2fc
deus <not.entari@gmail.com>
2014-05-18 19:00:04 +0300

    Added one function to Prim's algortihm. (prim)

--- Prim.py
+++ Prim.py
@@ -18,9 +18,34 @@ def decrease_key(Q):
     pass
 
 
-def prim(V):
+def prim(V, A, r):
+    u = 0
+    v = 0
 
-    pass
+    P=[None]*len(V)
+
+    K = [999999]*len(V)
+
+    Q=[0]*len(V)
+    for u in range(len(Q)):
+        Q[u] = V[u]
+
+    K[r] = 0
+    decreaseKey(Q, K)
+
+    while len(Q) > 0:
+        u = extractMin(Q)   
+
+        Adj = adjacent(A, u)
+        for v in Adj:
+            w = weight(A, u, v)
+
+
+            if Q.count(v)>0 and w < K[v]:
+                P[v] = u
+                K[v] = w
+                decreaseKey(Q, K)
+    return P
 
 
 print ("galutinis rezultatas")
